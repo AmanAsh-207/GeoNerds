fef6bbb0b00a69affacfd1598e7a2a8922fe0c9b
Vaibhav-hub123 <harshagrahri1256.ha@gmail.com>
2026-02-09 04:48:39 +0530

    added rotation

--- main_menu.gd
+++ main_menu.gd
@@ -2,14 +2,28 @@ extends Control
 
 @onready var offWale: Node2D = $Node2D
 
+@onready var red: ColorRect = $red
+@onready var blue: ColorRect = $Blue
+
+
 
 @onready var hard: Button = $hard
 @onready var easy: Button = $easy
+
+@onready var compass: Button = $compass
+
+var is_spinning = false
+var spin_speed = 300
+
 
 #func _ready():
 	## Update mode button text when menu opens
 	#$Node2D/difficulty_button.text = "Mode: " + GameSettings.game_mode.to_upper()
 #
+func _process(delta):
+	if is_spinning:
+		compass.rotation_degrees += spin_speed * delta
+
 #func _on_mode_selector_pressed() -> void:
 	#if GameSettings.game_mode == "easy":
 		#GameSettings.game_mode = "hard"
@@ -21,18 +35,31 @@ extends Control
 func _on_start_pressed() -> void:
 	offWale.visible = false
 	hard.visible = true
-	easy.visible = true
+	easy.visible = true 
+	is_spinning = true
 	#await get_tree().create_timer(5.0).timeout
 	#get_tree().change_scene_to_file("res://geonerds.tscn")
 
 
+func stop_and_select(target_angle, mode):
+	is_spinning = false
 
+	var tween = create_tween()
+	tween.tween_property(compass, "rotation_degrees", target_angle, 1).set_trans(Tween.TRANS_BACK)
+
+	await tween.finished
+
+	GameSettings.game_mode = mode
+	
+		
+	await get_tree().create_timer(1.0).timeout
+	get_tree().change_scene_to_file("res://geonerds.tscn")
+ 
 
 func _on_easy_pressed() -> void:
-	GameSettings.game_mode = "easy"
-	get_tree().change_scene_to_file("res://geonerds.tscn")
+		stop_and_select(-45, "easy")
+
 
 
 func _on_hard_pressed() -> void:
-	GameSettings.game_mode = "hard"
-	get_tree().change_scene_to_file("res://geonerds.tscn")
+	stop_and_select(45, "hard")

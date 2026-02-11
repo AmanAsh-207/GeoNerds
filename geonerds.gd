0403435edf23d0e504568b4e9891a41328d85136
Vaibhav-hub123 <harshagrahri1256.ha@gmail.com>
2026-02-11 20:37:51 +0530

    delay

--- geonerds.gd
+++ geonerds.gd
@@ -707,8 +707,10 @@ func game_Over()->void:
 	
 
 func _on_exit_pressed() -> void:
+	await get_tree().create_timer(0.5).timeout
 	get_tree().change_scene_to_file("res://main_menu.tscn")
 
 
 func _on_play_again_pressed() -> void:
+	await get_tree().create_timer(0.5).timeout
 	get_tree().change_scene_to_file("res://geonerds.tscn")
